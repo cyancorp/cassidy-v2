d53c4aa2d746387cedb3b16e868b21626a863cb6
cyan corp <cyan@corp>
2025-06-18 12:32:37 +0100

    make mobile app record a bit longer, still times out after 1m

--- mobile_flutter/macos/Flutter/GeneratedPluginRegistrant.swift
+++ mobile_flutter/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,7 @@
 import FlutterMacOS
 import Foundation
 
+import device_info_plus
 import path_provider_foundation
 import shared_preferences_foundation
 import speech_to_text_macos
@@ -13,6 +14,7 @@ import url_launcher_macos
 import video_player_avfoundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SpeechToTextMacosPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextMacosPlugin"))
